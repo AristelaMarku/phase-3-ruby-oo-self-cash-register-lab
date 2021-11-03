a52c9e1fe18366fef029d17b090f43cbc905b9a0
Aristela Marku <aristela.marku@gmail.com>
2021-11-03 17:36:54 +0100

    commit tree 34637c457d6cd13c849aeda51012d9181cf36fea

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
 
+require 'pry'
+
+class CashRegister
+
+   
+
+    attr_accessor :total, :discount, :items
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount  
+        @items=[]
+
+    end
+
+
+    def  add_item (title, price,quantity = 1)
+        self.total+=price*quantity
+        @items<<title
+    end
+    
+    def apply_discount
+        if  @discount==0
+            "There is no discount to apply."
+        else 
+            self.total-=self.total * @discount /100
+            "After the discount, the total comes to $#{self.total}."
+        end
+    end
+
+
+
+end
+
+
+ stela=CashRegister.new(10)
